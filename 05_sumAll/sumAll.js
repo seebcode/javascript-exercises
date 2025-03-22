e715b7b6277f75b8152135e2f314dde6646e177a
seebcode <155975312+seebcode@users.noreply.github.com>
2025-03-22 20:00:39 +1100

    Complete js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (
+        x < 0 || y < 0 || 
+        !(typeof x === 'number' && !Number.isNaN(x)) || 
+        !(typeof y === 'number' && !Number.isNaN(y)) ||
+        !(Number.isSafeInteger(x)) || !(Number.isSafeInteger(y))
+    ) {
+        return "ERROR";
+    }
 
+    // if first number is greater than last number, numbers should be swapped to ensure loop executes correctly
+    if (x > y) {
+        const temp = x;
+        x = y;
+        y = temp;
+    }
+
+    let sum = 0;
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
