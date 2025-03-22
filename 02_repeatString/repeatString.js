d90783e9722648bed9ab90d9d5052b286b9c16a7
seebcode <155975312+seebcode@users.noreply.github.com>
2025-03-22 17:31:45 +1100

    Complete js exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeats) {
+    if (numRepeats < 0) {
+        return "ERROR";
+    }
+    
+    let repeatedString = ""
+    for (let i = 0; i < numRepeats; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
